d26e9b16e9a375771ec29eab33df12ceb699f728
Bemoty <jos.winkler@tsn.at>
2020-04-12 21:47:59 +0200

    document ready

--- site/main.js
+++ site/main.js
@@ -1,36 +1,41 @@
-/**
- * Creates a new interactive world map.
- */
-var map = L.map('map').setView([47.2692, 11.4041], 8);
-
-/**
- * Creates tiles at the bottom right of the map.
- * These offer a reset button and display the selected date.
- */
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '<a id="resetOrientation" href="#">Reset</a> | Showing flights from <span id="dateTile"></span>',
-}).addTo(map);
-
-/**
- * Changes the tiles text to the currently selected date.
- */
-$('#datePicker').change(() => {
-    var date = new Date($('#datePicker').val());
-    $('#dateTile').text([date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/'))
-})
-
-/**
- * Resets the orientation of the map to it's original position 
- * upon clicking the 'Reset' anchor in the tile section, 
- */
-$('#resetOrientation').click(() => {
-    map.setView([47.2692, 11.4041], 8);
-})
-
+var map;
 var xmlHttp;
 
-map.on("moveend", () => {
-    const date = document.getElementById('datePicker').value;
+$(document).ready(() => {
+    // Create new interactive map focusing on Tyrol
+    map = L.map('map').setView([47.2692, 11.4041], 8);
+    L.circle([47.2692, 11.4041], { radius: 5000 }).addTo(map); // Test
+
+    // Tile buttons at bottom right corner of the map
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '<a id="resetOrientation" href="#">Reset</a> | Showing flights from <span id="dateTile"></span>',
+    }).addTo(map);
+    $('#resetOrientation').click(() => {
+        // Resets map position
+        map.setView([47.2692, 11.4041], 8);
+    });
+
+    map.on("moveend", () => updateMapElements());
+    $('#datePicker').change(() => updateDate());
+
+    // Initial load
+    updateMapElements();
+    updateDate();
+});
+
+/**
+ * Run when the date in the date picker updates
+ */
+function updateDate() {
+    var date = new Date($('#datePicker').val());
+    $('#dateTile').text([date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/'));
+}
+
+/**
+ * Updates visible elements on the map (i. e. aircraft)
+ */
+function updateMapElements() {
+    const date = $('#datePicker').value;
     const bound1 = map.getBounds().getNorthWest();
     const bound2 = map.getBounds().getSouthEast();
     console.log("Moved to: " + bound1 + " and " + bound2);
@@ -38,7 +43,7 @@ map.on("moveend", () => {
         xmlHttp.abort(); // abort last request
     }
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
+    xmlHttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(xmlHttp.responseText);
             for (let i = 0; i < response.length; i++) {
@@ -50,4 +55,4 @@ map.on("moveend", () => {
     };
     xmlHttp.open('GET', `http://localhost:1234/data/{"lat1": ${bound1.lat}, "lon1": ${bound1.lng}, "lat2": ${bound2.lat}, "lon2": ${bound2.lng}}/${date}`);
     xmlHttp.send();
-});
+}
